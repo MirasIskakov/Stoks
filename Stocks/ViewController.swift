2bf338bc50264281e960c1e13006703f2c63c4ec
MirasIskakov <iskakov91633@gmail.com>
2023-05-11 21:25:26 +0600

    d

---
+++ Stocks/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by swift on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Stocks"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(StocksTableViewCell.nib, forCellReuseIdentifier: StocksTableViewCell.identifier)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 52
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: StocksTableViewCell.identifier) as! StocksTableViewCell
+        
+        return cell
+    }
+    
+}
